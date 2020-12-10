9549da33c545f1064d46c39ec8a457fed798c856
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-10 11:41:53 -0500

    [list] add class component

---
+++ src/Components/List.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>List</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default List
